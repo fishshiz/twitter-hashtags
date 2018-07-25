63edb944005d2f1575623f66b915153054c07a86
Edan Lewis <edan@lewisarts.com>
2018-07-25 10:17:36 -0700

    heroku config

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,4 @@ const router = express.Router();
 const searchController = require("../controllers/searchController");
 
 router.get("/api/search", searchController.searchTweets);
-
-router.get("/", function(req, res, next) {
-  res.status(200);
-});
 module.exports = router;
