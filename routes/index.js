ddccb78508857bb8f93d175088119c3f367eb3a8
Edan Lewis <edan@lewisarts.com>
2018-07-25 10:27:56 -0700

    heroku build script

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,9 @@ const express = require("express");
 const router = express.Router();
 const searchController = require("../controllers/searchController");
 
+router.get("/", function(req, res, next) {
+  res.status(200).send("Hi, It works!");
+});
+
 router.get("/api/search", searchController.searchTweets);
 module.exports = router;
--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const routes = require("./routes/index");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use("/", routes);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
